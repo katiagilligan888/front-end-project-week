b0695ebe02ee1d5ebda8055ca650e65907d24c19
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-26 11:22:13 -0600

    Added UI to sideNav

--- lambda-notes/src/Components/SideNav.js
+++ lambda-notes/src/Components/SideNav.js
@@ -14,17 +14,17 @@ class SideNav extends React.Component {
   render() {
     return (
       <div style={this.props.style} className="side-nav">
-        <h1>Lambda Notes</h1>
-        <Link to="/notes" className=" button view-notes-button">
+        <h1>QuickNotes</h1>
+        <Link to="/notes" className="button">
           View Your Notes
         </Link>
-        <Link to="/new" className=" button create-note-button">
+        <Link to="/new" className="button">
           + Create New Note
         </Link>
         <Link
           to="/"
           onClick={this.signOutHandler}
-          className="button sign-out-button">
+          className="button">
           Sign Out
         </Link>
       </div>
